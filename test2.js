8362935fc983e5ec3af3e928ac84f2e366bb7610
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-07-20 23:31:20 +0300

    Test 2 File

--- test2.js
+++ test2.js
@@ -1,1 +1,14 @@
-console.log('This is a test!');
+function solve() {
+    let person = {
+        firstName: "Peter",
+        lastName: "Peterson",
+        age: "18",
+        salary: "7000$"
+    }
+
+    for (const key in person) {
+        console.log(key);
+    }
+}
+
+solve();
