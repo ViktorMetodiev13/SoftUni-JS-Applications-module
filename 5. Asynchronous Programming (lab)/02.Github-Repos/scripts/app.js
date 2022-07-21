f244c1ac624d83a7750e5639bb05a57333309a8e
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-07-21 17:44:07 +0300

    Solved Problem 3 and Made Changes on Problem 2

--- 5. Asynchronous Programming (lab)/02.Github-Repos/scripts/app.js
+++ 5. Asynchronous Programming (lab)/02.Github-Repos/scripts/app.js
@@ -22,6 +22,8 @@ async function loadRepos() {
 			list.appendChild(li);
 		}
 	} catch (error) {
-		list.innerHTML = `${error.message}`;
+		let li = document.createElement('li');
+		li.textContent = `${error.message}`;
+		list.appendChild(li);
 	}
 }
--- 5. Asynchronous Programming (lab)/03.Gighub-Commits/app.js
+++ 5. Asynchronous Programming (lab)/03.Gighub-Commits/app.js
@@ -1,16 +1,30 @@
 async function loadCommits() {
     let username = document.getElementById('username').value;
     let repo = document.getElementById('repo').value;
+    let list = document.getElementById('commits');
 
     try {
         let response = await fetch(`https://api.github.com/repos/${username}/${repo}/commits`);
         
         if (response.ok == false) {
-			throw new Error(`Error: ${response.status} ${response.statusText}`);
+			throw new Error(`${response.status} (${response.statusText})`);
 		}
-    } catch (error) {
-        
-    }
 
+        let data = await response.json();
+
+        list.textContent = '';
+
+        console.log(data);
+
+        for (let {commit} of data) {
+            let li = document.createElement('li');
+            li.textContent = `${commit.author.name}: ${commit.message}`;
+            list.appendChild(li);
+        }
+    } catch (error) {
+        let li = document.createElement('li');
+        li.textContent = `${error.message} (Not Found)`;
+        list.appendChild(li);
+    }
 
 }
