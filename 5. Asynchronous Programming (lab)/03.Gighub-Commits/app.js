b4844536c669b47abb5289c9a6e86999dc5e7ab5
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-07-21 17:20:05 +0300

    Changes on Problems 2 and 3

--- 5. Asynchronous Programming (lab)/03.Gighub-Commits/app.js
+++ 5. Asynchronous Programming (lab)/03.Gighub-Commits/app.js
@@ -1,4 +1,16 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+    let username = document.getElementById('username').value;
+    let repo = document.getElementById('repo').value;
+
+    try {
+        let response = await fetch(`https://api.github.com/repos/${username}/${repo}/commits`);
+        
+        if (response.ok == false) {
+			throw new Error(`Error: ${response.status} ${response.statusText}`);
+		}
+    } catch (error) {
+        
+    }
+
+
 }
