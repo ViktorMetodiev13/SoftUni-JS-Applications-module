7bf45807e3782878f677aee64e7bc57c317573c6
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-07-23 15:52:22 +0300

    Problem 1 half solution

--- 6. Asynchronous Programming (exe)/01.Bus-Stop/app.js
+++ 6. Asynchronous Programming (exe)/01.Bus-Stop/app.js
@@ -1,3 +1,20 @@
 function getInfo() {
-    console.log("TODO...");
+    let baseUrl = 'http://localhost:3030/jsonstore/bus/businfo';
+    let inputField = document.getElementById('stopId');
+    let listOfBuses = document.getElementById('buses');
+    let divElement = document.getElementById('stopName');
+    
+    fetch(`${baseUrl}/${inputField.value}`)
+        .then(res => res.json())
+        .then(data => {
+            let buses = data.buses;
+            let name = data.name;
+
+            divElement.textContent = name;
+
+            console.log(buses);
+        })
+        // .catch(err => {
+        //     divElement.textContent = 'Error';
+        // })
 }
