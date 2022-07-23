9954d1f80da7f0b4965818325fbd61a5c451d6d9
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-07-23 19:21:35 +0300

    Solution on Problem 1

--- 6. Asynchronous Programming (exe)/01.Bus-Stop/app.js
+++ 6. Asynchronous Programming (exe)/01.Bus-Stop/app.js
@@ -1,7 +1,7 @@
 function getInfo() {
     let baseUrl = 'http://localhost:3030/jsonstore/bus/businfo';
     let inputField = document.getElementById('stopId');
-    let listOfBuses = document.getElementById('buses');
+    let ulElement = document.getElementById('buses');
     let divElement = document.getElementById('stopName');
     
     fetch(`${baseUrl}/${inputField.value}`)
@@ -9,12 +9,17 @@ function getInfo() {
         .then(data => {
             let buses = data.buses;
             let name = data.name;
-
+            
             divElement.textContent = name;
-
-            console.log(buses);
+            ulElement.innerHTML = '';
+            Object.keys(buses).forEach(bus => {
+                let li = document.createElement('li');
+                li.textContent = `Bus ${bus} arrives in ${buses[bus]} minutes`;
+                ulElement.appendChild(li);
+            })
+            
         })
-        .catch(err => {
-            divElement.textContent = 'Error';
-        })
+        // .catch(err => {
+        //     divElement.textContent = 'Error';
+        // })
 }
