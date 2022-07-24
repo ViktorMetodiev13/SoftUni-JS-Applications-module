ffc0ee89a6946831b5734f585b675e6ab8fedee5
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-07-24 13:47:26 +0300

    Changes on Problem 2

--- 6. Asynchronous Programming (exe)/02.Bus-Schedule/app.js
+++ 6. Asynchronous Programming (exe)/02.Bus-Schedule/app.js
@@ -1,4 +1,4 @@
-function solve() {
+async function solve() {
     let baseUrl = 'http://localhost:3030/jsonstore/bus/schedule';
     let infoElement = document.querySelector('.info');
     let departBtn = document.getElementById('depart');
@@ -8,22 +8,19 @@ function solve() {
         next: 'Depot'
     }
 
-    let url = `http://localhost:3030/jsonstore/bus/schedule/${busStop.next}`;
-    function depart() {
+    async function depart() {
+        let url = `http://localhost:3030/jsonstore/bus/schedule/${busStop.next}`;
+        let res = await fetch(url);
+        let data = await res.json();
         departBtn.disabled = true;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                busStop = JSON.parse(JSON.stringify(data));
-                infoElement.textContent = `Next stop ${busStop.name}`;
-            })
+
+        busStop = JSON.parse(JSON.stringify(data));
+        infoElement.textContent = `Next Stop ${busStop.name}`;
         arriveBtn.disabled = false;
     }
 
-    function arrive() {
-        infoElement.textContent = `Arriving at ${busStop.next}`;
-
-        departBtn.disabled = false;
+    async function arrive() {
+        
     }
 
     return {
