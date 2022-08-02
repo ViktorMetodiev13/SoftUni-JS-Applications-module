aad95cc69a00452f9f111d6ac62b0bd4e4d87b6b
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-08-02 17:18:14 +0300

    Problem 2 onLoad function Finished

--- 8. Remote Data and Authentication (exe)/02.Phonebook/app.js
+++ 8. Remote Data and Authentication (exe)/02.Phonebook/app.js
@@ -1,5 +1,41 @@
 function attachEvents() {
-    console.log('TODO...');
+    const baseUrl = 'http://localhost:3030/jsonstore/phonebook';
+    document.querySelector('#btnLoad').addEventListener('click', onLoad);
+    document.querySelector('#btnCreate').addEventListener('click', onCreate);
+    let phonebook = document.querySelector('#phonebook');
+
+    async function onCreate() {
+
+    }
+
+    async function onLoad() {
+        try {
+            let res = await fetch(baseUrl);
+            if (res.ok == false) {
+                throw new Error(error.message);
+            } 
+
+            let data = await res.json();
+            phonebook.replaceChildren();
+
+            Object.values(data).forEach(p => {
+                let liElement = document.createElement('li');
+                liElement.textContent = `${p.person}: ${p.phone}`;
+                let buttonDelete = document.createElement('button');
+                buttonDelete.textContent = 'Delete';
+                liElement.appendChild(buttonDelete);
+                phonebook.appendChild(liElement);
+            })
+
+        } catch (error) {
+            alert(error.message);
+        }
+        
+    }
+
+    async function remove() {
+
+    }
 }
 
 attachEvents();
