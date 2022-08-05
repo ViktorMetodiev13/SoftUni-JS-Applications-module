79e30593ec105f4dfdaedb86f6d816585a792b0f
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-08-06 02:37:39 +0300

    Added functionalities and made Changes

--- 20. Exam Preparation/src/app.js
+++ 20. Exam Preparation/src/app.js
@@ -1,9 +1,36 @@
-let hostname = 'http://localhost:3030';
+import { html, render } from "../node_modules/lit-html/lit-html.js";
+import { getUserData } from "./util.js";
+import { page } from "../node_modules/page/page.mjs";
+import { logout } from "./api.js";
 
-async function request(url, options) {
-    try {
-        let response = await fetch(hostname, + url, options)
-    } catch (error) {
-        
+let root = document.getElementById('site-content');
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root);
+    ctx.updateUserNav = updateUserNav;
+
+    next();
+}
+
+export function updateUserNav() {
+    let userData = getUserData();
+
+    if (userData == true) {
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none';
+        document.querySelector('#user span').textContent = `Welcome, ${userData.email}`;
+    } else {
+        document.getElementById('guest').style.display = 'inline-block';
+        document.getElementById('user').style.display = 'none';
     }
 }
+
+document.getElementById('logoutBtn').addEventListener('click', () => {
+    logout();
+    updateUserNav();
+    page.redirect('/');
+})
+
+page(decorateContext)
+updateUserNav();
+page.start()
