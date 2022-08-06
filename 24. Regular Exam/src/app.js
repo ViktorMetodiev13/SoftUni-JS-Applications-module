93828f2a0ec9946b7b949f51428f9c10bbfc1e6e
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-08-06 09:47:38 +0300

    Added configurations

--- 24. Regular Exam/src/app.js
+++ 24. Regular Exam/src/app.js
@@ -1,3 +1,35 @@
 import { html, render } from "../node_modules/lit-html/lit-html.js";
+import { getUserData } from "./util.js";
+import { page } from "../node_modules/page/page.mjs";
+import { logout } from "./api.js";
 
-console.log('hey');
+let root = document.getElementById('site-content');
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root);
+    ctx.updateUserNav = updateUserNav;
+
+    next();
+}
+
+export function updateUserNav() {
+    let userData = getUserData();
+
+    if (userData == true) {
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none';
+    } else {
+        document.getElementById('guest').style.display = 'inline-block';
+        document.getElementById('user').style.display = 'none';
+    }
+}
+
+document.getElementById('logoutBtn').addEventListener('click', () => {
+    logout();
+    updateUserNav();
+    page.redirect('/');
+})
+
+page(decorateContext)
+updateUserNav();
+page.start()
