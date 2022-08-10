46cdb03e8e92157a5a3fd18760b5184c799b3550
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-08-10 18:53:38 +0300

    Changes on the Html file

--- ZZZ. Free Coding/03. Free Coding.js
+++ ZZZ. Free Coding/03. Free Coding.js
@@ -1,11 +1,1 @@
-async function solve() {
-
-    let url = 'http://localhost:3030';
-
-    let res = await fetch(url);
-    let data = await res.json();
-
-    console.log(data);
-}
-
-solve()
+console.log('hello');
