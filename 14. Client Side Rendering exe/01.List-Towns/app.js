0806a34a05e48fafdaa9141f762d2479cb338c9a
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-08-12 12:00:56 +0300

    Problem 1 solution

---
+++ 14. Client Side Rendering exe/01.List-Towns/app.js
@@ -0,0 +1,17 @@
+import { html, render } from "../node_modules/lit-html/lit-html.js";
+
+
+document.getElementById('btnLoadTowns').addEventListener('click', (e) => {
+    e.preventDefault();
+    let input = document.querySelector('[name="towns"]').value;
+    
+    let townsList = input.split(', ');
+    let cardTemplate = html`
+    <ul>
+    ${townsList.map(t => html `<li>${t}</li>`)};
+    </ul>
+    `;
+
+    let root = document.getElementById('root');
+    render(cardTemplate, root);
+})
