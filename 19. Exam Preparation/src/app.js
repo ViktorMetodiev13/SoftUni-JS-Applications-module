0c3f1ef171b21cbbc01a6e37f8c9a3dd0c3de046
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-08-14 20:47:43 +0300

    Home Page view done

--- 19. Exam Preparation/src/app.js
+++ 19. Exam Preparation/src/app.js
@@ -1,16 +1,16 @@
-import * as api from "./api/users.js";
-import { html, render } from "../node_modules/lit-html/lit-html.js";
-import page from "../node_modules/page/page.mjs";
+import { page, render } from "./lib.js";
+import { homeView } from "./views/home.js";
+import { loginView } from "./views/login.js";
 
 
-page('/', () => console.log('home'));
-page('/memes'), () => console.log('catalog');
-page('/memes/:id'), () => console.log('details');
-page('/edit/:id'), () => console.log('edit');
-page('/login'), () => console.log('login');
-page('/register'), () => console.log('register');
-page('/create'), () => console.log('create');
-page('/profile'), () => console.log('profile');
+page('/', homeView);
+page('/memes', loginView);
+page('/memes/:id', () => console.log('details'));
+page('/edit/:id', () => console.log('edit'));
+page('/login', () => console.log('login'));
+page('/register', () => console.log('register'));
+page('/create', () => console.log('create'));
+page('/profile', () => console.log('profile'));
 
 
 page.start();
