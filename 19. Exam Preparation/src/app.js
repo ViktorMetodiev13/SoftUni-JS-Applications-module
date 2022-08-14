5a7dfb28261c156f239dc4e61d4a7898e74e7661
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-08-14 23:26:51 +0300

    details functionality done

--- 19. Exam Preparation/src/app.js
+++ 19. Exam Preparation/src/app.js
@@ -3,13 +3,14 @@ import { page, render } from "./lib.js";
 import { clearUserData, getUserData } from "./util.js";
 import { catalogView } from "./views/catalog.js";
 import { createView } from "./views/create.js";
+import { detailsView } from "./views/details.js";
 import { homeView } from "./views/home.js";
 import { loginView } from "./views/login.js";
 import { registerView } from "./views/register.js";
 
 page('/', homeView);
 page('/memes', catalogView);
-page('/memes/:id', () => console.log('details'));
+page('/memes/:id', detailsView);
 page('/edit/:id', () => console.log('edit'));
 page('/login', loginView);
 page('/register', registerView);
