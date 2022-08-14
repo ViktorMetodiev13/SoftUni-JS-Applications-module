2c4b3a0d2bc811ec3028efd0104ffc2b5bc0c5ea
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-08-14 20:55:33 +0300

    catalog view partly done

--- 19. Exam Preparation/src/app.js
+++ 19. Exam Preparation/src/app.js
@@ -1,13 +1,14 @@
 import { page, render } from "./lib.js";
+import { catalogView } from "./views/catalog.js";
 import { homeView } from "./views/home.js";
 import { loginView } from "./views/login.js";
 
 
 page('/', homeView);
-page('/memes', loginView);
+page('/memes', catalogView);
 page('/memes/:id', () => console.log('details'));
 page('/edit/:id', () => console.log('edit'));
-page('/login', () => console.log('login'));
+page('/login', loginView);
 page('/register', () => console.log('register'));
 page('/create', () => console.log('create'));
 page('/profile', () => console.log('profile'));
